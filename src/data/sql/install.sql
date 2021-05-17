0eb0e3f0b16e54c31c13931f8d3e1632dfb3d88d
陈庙琴 <1421224516@qq.com>
2021-05-17 10:19:34 +0800

    feat: composer.json添加插件配置 (refs: #210517-1019)

--- src/data/sql/install.sql
+++ src/data/sql/install.sql
@@ -4,11 +4,10 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 -- Table structure for epii_image
 -- ----------------------------
-DROP TABLE IF EXISTS `epii_image`;
-CREATE TABLE `epii_image`  (
+CREATE TABLE IF NOT EXISTS `epii_image`  (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '图片表主键id',
   `photo_name` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '图片名称',
-  `url` varchar(156) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '图片url',
+  `url` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '图片url',
   `description` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '描述',
   `note` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '备注',
   `create_time` int(10) UNSIGNED NULL DEFAULT 0 COMMENT '创建时间',
@@ -19,8 +18,7 @@ CREATE TABLE `epii_image`  (
 -- ----------------------------
 -- Table structure for epii_log_making
 -- ----------------------------
-DROP TABLE IF EXISTS `epii_log_making`;
-CREATE TABLE `epii_log_making`  (
+CREATE TABLE IF NOT EXISTS `epii_log_making`  (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '生成图片记录表主键id',
   `image_id` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '图片表外键id',
   `image_url` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '图片url',
@@ -35,8 +33,7 @@ CREATE TABLE `epii_log_making`  (
 -- ----------------------------
 -- Table structure for epii_tag
 -- ----------------------------
-DROP TABLE IF EXISTS `epii_tag`;
-CREATE TABLE `epii_tag`  (
+CREATE TABLE IF NOT EXISTS `epii_tag`  (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '标签表主键id',
   `tag_name` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '标签名称',
   `status` tinyint(3) UNSIGNED NOT NULL DEFAULT 0 COMMENT '状态：0-禁用；1-启用',
@@ -48,8 +45,7 @@ CREATE TABLE `epii_tag`  (
 -- ----------------------------
 -- Table structure for epii_template
 -- ----------------------------
-DROP TABLE IF EXISTS `epii_template`;
-CREATE TABLE `epii_template`  (
+CREATE TABLE IF NOT EXISTS `epii_template`  (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '模板表主键id',
   `template_name` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '模板名称',
   `size_id` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '尺寸表外键id',
@@ -63,8 +59,7 @@ CREATE TABLE `epii_template`  (
 -- ----------------------------
 -- Table structure for epii_template_tag
 -- ----------------------------
-DROP TABLE IF EXISTS `epii_template_tag`;
-CREATE TABLE `epii_template_tag`  (
+CREATE TABLE IF NOT EXISTS `epii_template_tag`  (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '模板-标签表主键id',
   `template_id` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '模板表外键id',
   `tag_id` int(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT '标签表外键id',
